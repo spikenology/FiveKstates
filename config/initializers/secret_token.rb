d02946184e3a77ab031d8381518398f8799cbfd9
JOhn Schweikart <spikenology@gmail.com>
2013-09-18 10:21:23 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FiveKstates::Application.config.secret_key_base = '3234ce1e6f58d1179dafafd306c130fb437146596486362ff172879e4c90062d3280f81c18df23457dec6598ebd81437092d56b8a3fb17511cba76f7b04a0a19'
